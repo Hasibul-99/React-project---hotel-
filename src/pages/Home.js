2f38f540dde49f792c19c81fce2b7bf277f569b7
Hasibul-99 <hasibul_uap@outlook.com>
2019-05-23 22:12:03 +0600

    Banner implement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      hello from the home pag  e
-    </div>
+    <Hero>
+      <Banner title="Luxurious rooms" subtitle="Deluxe rooms start ing at $299">
+        <Link to="/rooms" className="btn-primary">Our rooms</Link>
+      </Banner>
+    </Hero>
   )
 }
 
